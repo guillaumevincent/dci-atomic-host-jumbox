4496cd018063abea19721fa72164f4d7caa7bdce
Guillaume Vincent <gvincent@redhat.com>
2017-09-22 09:29:43 +0200

    Use default env variable

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ git checkout master
 git reset --hard origin/master
 source ./openrc.sh
 source ./dcirc.sh
-ansible-playbook -e topic='Fedora-Atomic-26' -e SSH_HOST='38.145.32.193' -e SSH_USER='fedora' -e SSH_PRIVATE_KEY_FILE='/home/centos/.ssh/id_rsa' -e SSH_PUBLIC_KEY_FILE='/home/centos/.ssh/id_rsa.pub' dci-atomic-host-agent.yml
+ansible-playbook -e TOPIC_NAME=${TOPIC_NAME:='Fedora-Atomic-26'} -e SSH_HOST=${SSH_HOST:='38.145.32.193'} -e SSH_USER=${SSH_USER:='fedora'} -e SSH_PRIVATE_KEY_FILE=${SSH_PRIVATE_KEY_FILE:='/home/centos/.ssh/id_rsa'} -e SSH_PUBLIC_KEY_FILE=${SSH_PUBLIC_KEY_FILE:='/home/centos/.ssh/id_rsa.pub'} dci-atomic-host-agent.yml
 popd
